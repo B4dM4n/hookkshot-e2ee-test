4fa64e8fa5e8434c8f5a155e96b190063da5e4f4
Fabian Möller <fabianm88@gmail.com>
2022-08-01 18:08:16 +0200

    Update enctest source

--- enctest/src/index.ts
+++ enctest/src/index.ts
@@ -1,11 +1,6 @@
-import * as fs from "fs";
-
 import {
   Appservice,
   EncryptionAlgorithm,
-  FileMessageEventContent,
-  IAppserviceOptions,
-  IAppserviceRegistration,
   LogLevel,
   LogService,
   MessageEvent,
@@ -20,98 +15,86 @@ LogService.setLevel(LogLevel.TRACE);
 LogService.muteModule("Metrics");
 LogService.trace = LogService.debug;
 
-let creds = null;
-try {
-  creds = require("/data/encryption_appservice.creds.json");
-} catch (e) {
-  // ignore
-}
-
-const dmTarget = creds?.['dmTarget'] ?? "@admin:local";
-const homeserverUrl = creds?.['homeserverUrl'] ?? "http://synapse:8008";
-const storage = new SimpleFsStorageProvider("/data/encryption_appservice.json");
-const crypto = new RustSdkAppserviceCryptoStorageProvider("/data/encryption");
-
-const registration: IAppserviceRegistration = {
-  "as_token": creds?.['asToken'] ?? "change_me",
-  "hs_token": creds?.['hsToken'] ?? "change_me",
-  "sender_localpart": "encbot",
-  "namespaces": {
-    users: [{
-      regex: "@crypto.*:local",
-      exclusive: true,
-    }],
-    rooms: [],
-    aliases: [],
-  },
-  "de.sorunome.msc2409.push_ephemeral": true,
-};
-
-const options: IAppserviceOptions = {
+const appservice = new Appservice({
   bindAddress: "0.0.0.0",
   port: 9994,
   homeserverName: "local",
-  homeserverUrl: homeserverUrl,
+  homeserverUrl: "http://synapse:8008",
 
-  storage: storage,
-  registration: registration,
+  storage: new SimpleFsStorageProvider("/data/encryption_appservice.json"),
+  registration: {
+    "as_token": "change_me",
+    "hs_token": "change_me",
+    "sender_localpart": "encbot",
+    "namespaces": {
+      users: [{
+        regex: "@crypto_.*:local",
+        exclusive: true,
+      }],
+      rooms: [],
+      aliases: [],
+    },
+    "de.sorunome.msc2409.push_ephemeral": true,
+  },
   joinStrategy: new SimpleRetryJoinStrategy(),
-  cryptoStorage: crypto,
+  cryptoStorage: new RustSdkAppserviceCryptoStorageProvider("/data/encryption"),
 
   intentOptions: {
     encryption: true,
   },
-};
-
-const appservice = new Appservice(options);
+});
 const bot = appservice.botIntent;
-// const bot = appservice.getIntentForUserId("@crypto_bot1:local");
 
 (async function () {
   await bot.enableEncryption();
 
-  let encryptedRoomId: string;
+  let privateRoomId: string;
   const joinedRooms = await bot.underlyingClient.getJoinedRooms();
   for (const roomId of joinedRooms) {
     if (await bot.underlyingClient.crypto.isRoomEncrypted(roomId)) {
-      const members = await bot.underlyingClient.getJoinedRoomMembers(roomId);
-      if (members.length >= 2) {
-        encryptedRoomId = roomId;
-        break;
-      }
+      try {
+        const roomName = (await bot.underlyingClient.getRoomStateEvent(roomId, "m.room.name", ""))?.["name"];
+        if (roomName === "Encbot Private") {
+          privateRoomId = roomId;
+          break;
+        }
+      } catch (e) { }
     }
   }
-  if (!encryptedRoomId) {
-    encryptedRoomId = await bot.underlyingClient.createRoom({
-      invite: [dmTarget],
-      is_direct: true,
+  if (!privateRoomId) {
+    privateRoomId = await bot.underlyingClient.createRoom({
       visibility: "private",
-      preset: "trusted_private_chat",
+      preset: "private_chat",
       initial_state: [
         { type: "m.room.encryption", state_key: "", content: { algorithm: EncryptionAlgorithm.MegolmV1AesSha2 } },
-        { type: "m.room.guest_access", state_key: "", content: { guest_access: "can_join" } },
+        { type: "m.room.name", state_key: "", content: { name: "Encbot Private" } },
       ],
     });
   }
+
+  appservice.on("room.invite", async (roomId: string, inviteEvent: any) => {
+    LogService.info("index", `Received invite for ${inviteEvent["state_key"]} to ${roomId}`);
+    await bot.joinRoom(roomId);
+  });
+
+  appservice.on("room.join", (roomId: string, joinEvent: any) => {
+    LogService.info("index", `Joined ${roomId} as ${joinEvent["state_key"]}`);
+  });
 
   appservice.on("room.failed_decryption", async (roomId: string, event: any, e: Error) => {
     LogService.error("index", `Failed to decrypt ${roomId} ${event['event_id']} because `, e);
   });
 
   appservice.on("room.message", async (roomId: string, event: any) => {
-    if (roomId !== encryptedRoomId) return;
-
     const message = new MessageEvent(event);
 
-    // if (message.sender === bot.userId && message.messageType === "m.notice") {
-    //   // yay, we decrypted our own message. Communicate that back for testing purposes.
-    //   await bot.underlyingClient.replyNotice(roomId, event, "it works");
-    //   return;
-    // }
-
     if (message.messageType !== "m.text") return;
+
     if (message.textBody.startsWith("!ping")) {
       await bot.underlyingClient.replyNotice(roomId, event, "Pong");
+    } else if (message.textBody.startsWith("!invite")) {
+      await bot.underlyingClient.inviteUser(message.sender, privateRoomId);
+      await bot.underlyingClient.replyNotice(roomId, event, "Invited you into private room");
     }
   });
 
